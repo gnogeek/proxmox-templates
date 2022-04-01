54406d9f7c83b22b041c06edc9bd2d353b4c89ca
gnogeek <77178414+gnogeek@users.noreply.github.com>
2022-04-01 11:39:27 -0400

    Update ubuntu-bionic.sh

--- ubuntu-bionic.sh
+++ ubuntu-bionic.sh
@@ -2,6 +2,14 @@
 
 clear
 ### VM Storage
+clear
+echo "########## STORAGE ##########"
+echo ""
+echo Storage Availability|awk '{ printf "%-20s %-40s\n", $1, $2 }'
+pvesm status|grep active|awk '{ printf "%-20s %-40s\n", $1, $7 }'
+echo -n "Type name of Storage to install VM: "
+read TEMPLATE_VM_STORAGE
+
 # IMAGE PATH
 IMG_PATH="imgs"
 ### Check if imgs path exist
@@ -78,16 +86,16 @@ VMID="9005"
 CORES="2"
 MEM="1024"
 DISK_SIZE="8G"
-DISK_STOR="local-lvm"
+#DISK_STOR="local-lvm"
 NET_BRIDGE="vmbr0"
 #SRC_IMG="https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
 #IMG_NAME="bionic-server-cloudimg-amd64.qcow2"
 #wget -nc -O $IMG_NAME $SRC_IMG
 virt-customize -a $TEMPLATE_VM_CI_IMAGE --install qemu-guest-agent
 qm create $VMID --name $TEMPL_NAME --memory $MEM --net0 virtio,bridge=$NET_BRIDGE --core $CORES
-qm importdisk $VMID $TEMPLATE_VM_CI_IMAGE $DISK_STOR
-qm set $VMID --scsihw virtio-scsi-pci --scsi0 $DISK_STOR:vm-$VMID-disk-0
-qm set $VMID --ide2 $DISK_STOR:cloudinit
+qm importdisk $VMID $TEMPLATE_VM_CI_IMAGE $TEMPLATE_VM_STORAGE
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 $TEMPLATE_VM_STORAGE:vm-$VMID-disk-0
+qm set $VMID --ide2 $TEMPLATE_VM_STORAGE:cloudinit
 qm set $VMID --boot c --bootdisk scsi0
 qm set $VMID --serial0 socket --vga serial0
 qm set $VMID --ipconfig0 ip=dhcp
