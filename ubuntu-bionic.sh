4797fd175a8ecc6982f84dd8fb2077149fabe4d1
gnogeek <77178414+gnogeek@users.noreply.github.com>
2022-04-01 11:02:30 -0400

    Update ubuntu-bionic.sh

--- ubuntu-bionic.sh
+++ ubuntu-bionic.sh
@@ -1,5 +1,78 @@
 #!/bin/bash
 ### VM Storage
+# IMAGE PATH
+IMG_PATH="imgs"
+### Check if imgs path exist
+if [ ! -d $IMG_PATH ] ; then
+	mkdir -p $IMG_PATH
+fi
+
+#URLS - Available compatible cloud-init images to download - Debina 9/10 and Ubuntu 18.04/20.04
+DEBIAN_10_URL="https://cdimage.debian.org/cdimage/openstack/current-10/debian-10-openstack-amd64.raw"
+DEBIAN_9_URL="https://cdimage.debian.org/cdimage/openstack/current-9/debian-9-openstack-amd64.raw"
+UBUNTU_1804_URL="https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
+UBUNTU_2004_URL="https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
+OPENSUSE_152_URL="https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.2/images/openSUSE-Leap-15.2-OpenStack.x86_64.qcow2"
+CENTOS_8_URL="https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.1.1911-20200113.3.x86_64.qcow2"
+####
+echo "Available images are: "
+echo -n "
+1 - Debian 9 - Stretch
+2 - Debian 10 - Buster
+3 - Ubuntu 18.04 LTS - Bionic
+4 - Ubuntu 20.04 LTS - Focal
+5 - OpenSUSE LEAP 15.02
+6 - CentOS 8
+"
+echo -n "Choose a Image template to install: "
+read OPT_IMAGE_TEMPLATE
+
+case $OPT_IMAGE_TEMPLATE in
+	1)
+		TEMPLATE_VM_CI_IMAGE="$IMG_PATH/${DEBIAN_9_URL##*/}"
+		if [ ! -f $TEMPLATE_VM_CI_IMAGE ]; then
+			wget -nc $DEBIAN_9_URL -O $TEMPLATE_VM_CI_IMAGE
+		fi
+		;;
+	2)
+		TEMPLATE_VM_CI_IMAGE="$IMG_PATH/${DEBIAN_10_URL##*/}"
+		if [ ! -f $TEMPLATE_VM_CI_IMAGE ]; then
+			wget -nc $DEBIAN_10_URL -O $TEMPLATE_VM_CI_IMAGE
+		fi
+		;;
+	3)
+		TEMPLATE_VM_CI_IMAGE="$IMG_PATH/${UBUNTU_1804_URL##*/}"
+		if [ ! -f $TEMPLATE_VM_CI_IMAGE ]; then
+			wget -nc $UBUNTU_1804_URL -O $TEMPLATE_VM_CI_IMAGE
+		fi
+		;;
+	4)
+		TEMPLATE_VM_CI_IMAGE="$IMG_PATH/${UBUNTU_2004_URL##*/}"
+		if [ ! -f $TEMPLATE_VM_CI_IMAGE ]; then
+			wget -nc $UBUNTU_2004_URL -O $TEMPLATE_VM_CI_IMAGE
+		fi
+		;;
+	5)
+		TEMPLATE_VM_CI_IMAGE="$IMG_PATH/${OPENSUSE_152_URL##*/}"
+		if [ ! -f $TEMPLATE_VM_CI_IMAGE ]; then
+			wget -nc $OPENSUSE_152_URL -O $TEMPLATE_VM_CI_IMAGE
+		fi
+		;;
+	6)
+		TEMPLATE_VM_CI_IMAGE="$IMG_PATH/${CENTOS_8_URL##*/}"
+		if [ ! -f $TEMPLATE_VM_CI_IMAGE ]; then
+			wget -nc $CENTOS_8_URL -O $TEMPLATE_VM_CI_IMAGE
+		fi
+		;;
+	*)
+		clear
+		echo "[Fail] - Unknown option - Run script again then choose a valid option."
+		exit
+		;;
+esac
+clear
+
+####
 TEMPL_NAME="ubuntu18.04-cloud"
 VMID="9005"
 CORES="2"
@@ -7,12 +80,12 @@ MEM="1024"
 DISK_SIZE="8G"
 DISK_STOR="local-lvm"
 NET_BRIDGE="vmbr0"
-SRC_IMG="https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
-IMG_NAME="bionic-server-cloudimg-amd64.qcow2"
-wget -nc -O $IMG_NAME $SRC_IMG
-virt-customize -a $IMG_NAME --install qemu-guest-agent
+#SRC_IMG="https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
+#IMG_NAME="bionic-server-cloudimg-amd64.qcow2"
+#wget -nc -O $IMG_NAME $SRC_IMG
+virt-customize -a $TEMPLATE_VM_CI_IMAGE --install qemu-guest-agent
 qm create $VMID --name $TEMPL_NAME --memory $MEM --net0 virtio,bridge=$NET_BRIDGE --core $CORES
-qm importdisk $VMID $IMG_NAME $DISK_STOR
+qm importdisk $VMID $TEMPLATE_VM_CI_IMAGE $DISK_STOR
 qm set $VMID --scsihw virtio-scsi-pci --scsi0 $DISK_STOR:vm-$VMID-disk-0
 qm set $VMID --ide2 $DISK_STOR:cloudinit
 qm set $VMID --boot c --bootdisk scsi0
